b806d2cdf6228d8a2e02f258979c2326a3084ba7
slayer3103 <saurabhshendurkar3103@gmail.com>
2026-05-19 00:05:48 +0530

    fix mysql production connection

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,31 +5,22 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 10000
+  queueLimit: 0
 });
 
-// const mysql = require('mysql2');
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
-
-// module.exports = pool;
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME
+});
 
 pool.getConnection((err, connection) => {
   if (err) {
-    console.error('❌ Error connecting to MySQL pool:', err.message);
+    console.error('❌ Error connecting to MySQL pool:', err);
   } else {
     console.log('✅ MySQL Connection Pool established');
     connection.release();
