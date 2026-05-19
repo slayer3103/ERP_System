36dfbb5e402420f1c0f71265722f6b6efaa875af
slayer3103 <saurabhshendurkar3103@gmail.com>
2026-05-19 19:24:29 +0530

    add fix source in  cors origin

--- Backend/index.js
+++ Backend/index.js
@@ -10,8 +10,9 @@ const cors = require('cors');
 
 const allowedOrigins = [
   "https://erp-system-as6ukav32-saurabhshendurkar3103-8214s-projects.vercel.app",
-  "https://erp-system-saurabhshendurkar3103-8214s-projects.vercel.app/"
+  "https://erp-system-git-main-saurabhshendurkar3103-8214s-projects.vercel.app"
 ];
+
 
 app.use(cors({
   origin: function(origin, callback) {
